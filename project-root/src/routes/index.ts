c7c1bcb89e15849e54deb708ffb1cea5a1244b29
ManhVu04 <manhsd2004@gmail.com>
2025-03-23 00:14:57 +0700

    HoanChinhLan1

--- project-root/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-export function setRoutes(app) {
-    const IndexController = require('../controllers/index').IndexController;
-    const indexController = new IndexController();
-
-    app.get('/', indexController.home.bind(indexController));
-    app.get('/about', indexController.about.bind(indexController));
-    app.get('/contact', indexController.contact.bind(indexController));
-}
